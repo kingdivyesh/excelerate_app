c0be82a78b227e9d094e57f22b620426967248e9
kingdivyesh <kingdivyesh15@gmail.com>
2025-11-02 12:33:27 +0530

    Update build.gradle for release settings

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,10 +20,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        // TODO: Specify your own unique Application ID
         applicationId = "com.example.excelerate_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -31,9 +29,12 @@ android {
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+        getByName("release") {
+            // Use Kotlin DSL property syntax
+            isMinifyEnabled = true
+            isShrinkResources = true
+
+            // TODO: Replace with your release signing config for Play Store
             signingConfig = signingConfigs.getByName("debug")
         }
     }
